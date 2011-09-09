1ad21cc09ed9b1c97b4e50c9b0ab471d97fe8041
rkeene <rkeene@noemail.net>
2011-09-09 14:32:05 +0000

    Updated to correctly build release output
    Removed extraneous file
    
    FossilOrigin-Name: 9608a4d5cf4e07276771556728cef1952b934818

--- build/autogen.sh
+++ build/autogen.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+if [ -n "${VERS}" ]; then
+	sed "s|@@VERS@@|${VERS}|g" configure.ac > configure.ac.new
+	cat configure.ac.new > configure.ac
+	rm -f configure.ac.new
+fi
+
 cat aclocal/*.m4 > aclocal.m4 || exit 1
 autoconf || exit 1
 rm -rf autom4te.cache/
